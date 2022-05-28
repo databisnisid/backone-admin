5c3507febcde0e480cf7f3135c85b9d580ce125f
gkiserpong <gkiserpongit@gmail.com>
2022-05-28 10:22:56 +0700

    Add Summary on Map

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from backone.models import BackOne
+from connection.models import ConnectionStatus
 from django.conf import settings
 
 
 def index(request):
     backone = BackOne.objects.all()
+    cstatus = ConnectionStatus.objects.all()
 
-    return render(request, 'index.html', {'backone': backone, 'settings': settings})
+    return render(request, 'index.html', {
+        'backone': backone,
+        'cstatus': cstatus,
+        'settings': settings
+    })
