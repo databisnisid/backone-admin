c9b98720b8035a730c91d60c2c5106555647ec81
gkiserpong <gkiserpongit@gmail.com>
2022-06-09 13:08:01 +0700

    Print log orbit

--- orbit/utils.py
+++ orbit/utils.py
@@ -1,6 +1,7 @@
 from .models import Orbit
 from connector.drivers import orbit
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
 
 
 def get_quota_orbit(msisdn):
@@ -31,4 +32,4 @@ def get_all_quota_orbit():
             o.quota_total = q_total if len(q_total) != 0 else o.quota_total
             o.quota_day = q_day if len(q_day) != 0 else o.quota_day
             o.save()
-            print(o.msisdn, o.quota_current, o.quota_total, o.quota_day)
+            print(timezone.now, o.msisdn, o.quota_current, o.quota_total, o.quota_day)
