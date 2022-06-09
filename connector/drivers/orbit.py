6588f1d81ec41a4558a8bef61aa20b12cd355d77
gkiserpong <gkiserpongit@gmail.com>
2022-06-09 09:56:33 +0700

    Not initiate selenium if no username or no password

--- connector/drivers/orbit.py
+++ connector/drivers/orbit.py
@@ -7,61 +7,65 @@ from selenium.webdriver.common.keys import Keys
 from django.conf import settings
 
 
-def get_quota(username='budithegreat09@gmail.com', password='Failover6!'):
+#def get_quota(username='budithegreat09@gmail.com', password='Failover6!'):
+def get_quota(username, password):
 
-    print('Use Selenium Server: ', settings.REMOTE_SELENIUM)
-    if settings.REMOTE_SELENIUM:
-        #driver_options = webdriver.ChromeOptions()
-        driver_options = webdriver.FirefoxOptions()
-
-        driver = webdriver.Remote(
-            command_executor=settings.SELENIUM_DOCKER,
-            options=driver_options
-        )
-    else:
-        driver = webdriver.Firefox()
-
-    # Delete All Cookies ?
-    #driver.delete_all_cookies()
-
-    driver.get("https://www.myorbit.id/login")
-    delay = 10
     quota_current = ''
     quota_total = ''
     quota_day = ''
 
-    """ Sending Username """
-    try:
-        elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//input[contains(@label,'Alamat Email/No. HP')]"))
-        )
-        elem.send_keys(username)
-        elem.send_keys(Keys.RETURN)
-    except (NoSuchElementException, TimeoutException):
+    if username is not None and password is not None:
+
+        print('Use Selenium Server: ', settings.REMOTE_SELENIUM)
+        if settings.REMOTE_SELENIUM:
+            #driver_options = webdriver.ChromeOptions()
+            driver_options = webdriver.FirefoxOptions()
+
+            driver = webdriver.Remote(
+                command_executor=settings.SELENIUM_DOCKER,
+                options=driver_options
+            )
+        else:
+            driver = webdriver.Firefox()
+
+        # Delete All Cookies ?
+        #driver.delete_all_cookies()
+
+        driver.get("https://www.myorbit.id/login")
+        delay = 10
+
+        """ Sending Username """
+        try:
+            elem = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located((By.XPATH, "//input[contains(@label,'Alamat Email/No. HP')]"))
+            )
+            elem.send_keys(username)
+            elem.send_keys(Keys.RETURN)
+        except (NoSuchElementException, TimeoutException):
+            driver.quit()
+
+        """ Sending Password """
+        try:
+            elem = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located((By.XPATH, "//input[contains(@label,'Password')]"))
+            )
+            elem.send_keys(password)
+            elem.send_keys(Keys.RETURN)
+        except (NoSuchElementException, TimeoutException):
+            driver.quit()
+
+        """ Get Quota Information """
+        try:
+            elem = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located((By.ID, "package-meter-chart"))
+            )
+            quota_current = elem.find_element(By.XPATH, "./div[2]/div[2]/div[1]").text
+            quota_total = elem.find_element(By.XPATH, "./div[2]/div[2]/div[3]").text
+            elem = driver.find_element(By.XPATH, "//div[text()='Internet Orbit']")
+            quota_day = elem.find_element(By.XPATH, "./span").text
+        except (NoSuchElementException, TimeoutException):
+            pass
+
         driver.quit()
-
-    """ Sending Password """
-    try:
-        elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//input[contains(@label,'Password')]"))
-        )
-        elem.send_keys(password)
-        elem.send_keys(Keys.RETURN)
-    except (NoSuchElementException, TimeoutException):
-        driver.quit()
-
-    """ Get Quota Information """
-    try:
-        elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.ID, "package-meter-chart"))
-        )
-        quota_current = elem.find_element(By.XPATH, "./div[2]/div[2]/div[1]").text
-        quota_total = elem.find_element(By.XPATH, "./div[2]/div[2]/div[3]").text
-        elem = driver.find_element(By.XPATH, "//div[text()='Internet Orbit']")
-        quota_day = elem.find_element(By.XPATH, "./span").text
-    except (NoSuchElementException, TimeoutException):
-        pass
-
-    driver.quit()
 
     return quota_current, quota_total, quota_day
