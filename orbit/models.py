180ac5864009557ba4fbdaaef7d0234b2aec8a30
gkiserpong <gkiserpongit@gmail.com>
2022-06-09 10:11:34 +0700

    Fix Orbit and selenium

--- orbit/models.py
+++ orbit/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class Orbit(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    msisdn = models.CharField(max_length=30)
-    imei = models.CharField(max_length=30)
+    username = models.EmailField(max_length=50, default='nopass@backone.cloud')
+    password = models.CharField(max_length=50, default='nopassword')
+    msisdn = models.CharField(max_length=30, verbose_name='MSISDN')
+    imei = models.CharField(max_length=30, verbose_name='IMEI')
     quota_total = models.CharField(max_length=20, blank=True)
     quota_current = models.CharField(max_length=20, blank=True)
     quota_day = models.CharField(max_length=20, blank=True)
+
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
--- orbit/utils.py
+++ orbit/utils.py
@@ -6,9 +6,10 @@ def get_all_quota_orbit():
     orbits = Orbit.objects.all()
 
     for o in orbits:
-        q_current, q_total, q_day = orbit.get_quota(o.username, o.password)
-        o.quota_current = q_current if len(q_current) != 0 else o.quota_current
-        o.quota_total = q_total if len(q_total) != 0 else o.quota_total
-        o.quota_day = q_day if len(q_day) != 0 else o.quota_day
-        o.save()
-        print(o.quota_current, o.quota_total, o.quota_day)
+        if o.username != 'nopass@backone.cloud' or o.password != 'nopassword':
+            q_current, q_total, q_day = orbit.get_quota(o.username, o.password)
+            o.quota_current = q_current if len(q_current) != 0 else o.quota_current
+            o.quota_total = q_total if len(q_total) != 0 else o.quota_total
+            o.quota_day = q_day if len(q_day) != 0 else o.quota_day
+            o.save()
+            print(o.quota_current, o.quota_total, o.quota_day)
