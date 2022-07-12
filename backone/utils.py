0f737715e83e661fa0ece8902bb72f1ac1a9c7ad
gkiserpong <gkiserpongit@gmail.com>
2022-07-12 14:33:47 +0700

    validate ip address

--- backone/utils.py
+++ backone/utils.py
@@ -1,15 +1,27 @@
 from .models import BackOne
+from ipaddress import ip_address
 from connector.drivers import ping
 
 PING_PACKET = 1
 PING_TIMEOUT = 3
 
 
+def validate_ip_address(address):
+    try:
+        ip = ip_address(address)
+        return True
+    except ValueError:
+        return False
+
+
 def update_sites_ping_status():
     location = BackOne.objects.all()
     for loc in location:
-        if loc.ipaddress is not '0.0.0.0':
+        loc.ping_status = False
+
+        if validate_ip_address(loc.ipaddress):
             ping_result = ping.ping(loc.ipaddress, PING_PACKET, PING_TIMEOUT)
             loc.ping_status = ping_result
             print('PING', loc.ipaddress, ping_result)
-            loc.save()
+
+        loc.save()
