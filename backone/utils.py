eee0d3ec37dc3053dffc225466a4c253173fe811
gkiserpong <gkiserpongit@gmail.com>
2022-07-12 14:38:27 +0700

    Check IP NULL

--- backone/utils.py
+++ backone/utils.py
@@ -4,6 +4,7 @@ from connector.drivers import ping
 
 PING_PACKET = 1
 PING_TIMEOUT = 3
+IP_NULL = '0.0.0.0'
 
 
 def validate_ip_address(address):
@@ -19,7 +20,7 @@ def update_sites_ping_status():
     for loc in location:
         loc.ping_status = False
 
-        if validate_ip_address(loc.ipaddress):
+        if validate_ip_address(loc.ipaddress) and loc.ipaddress != IP_NULL:
             ping_result = ping.ping(loc.ipaddress, PING_PACKET, PING_TIMEOUT)
             loc.ping_status = ping_result
             print('PING', loc.ipaddress, ping_result)
