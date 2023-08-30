9290a825b22015b0479684350601e74fba0d4934
gkiserpong <gkiserpongit@gmail.com>
2023-08-30 13:32:20 +0700

    query msisdn

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from landing.views import index
+from orbit.views import get_quota_all_orbit, get_quota_all_orbit_multi, get_quota
 
 
 urlpatterns = [
@@ -10,6 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sites/', include('backone.urls')),
     path('landing/', include('landing.urls')),
+    path('quota/all_orbit/', get_quota_all_orbit, name='all_orbit'),
+    path('quota/all_orbit_multi/', get_quota_all_orbit_multi, name='all_orbit_multi'),
+    path('quota/<str:msisdn>/', get_quota, name='get_quota'),
     #path('statistic/', include('statistic.urls')),
 ]
 
--- orbit/views.py
+++ orbit/views.py
@@ -1,2 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers import serialize
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Orbit, OrbitMulti
+
+
+def get_quota_all_orbit(request):
+    return JsonResponse(list(Orbit.objects.all().values()), safe=False)
+
+
+def get_quota_all_orbit_multi(request):
+    return JsonResponse(list(OrbitMulti.objects.all().values()), safe=False)
+
+
+def get_quota(request, msisdn):
+
+    qs = Orbit.objects.filter(msisdn__contains=msisdn)
+
+    if not qs:
+        qs = OrbitMulti.objects.filter(msisdn__contains=msisdn)
+
+    print(msisdn, qs)
+    serialized_qs = serialize('python', qs)
+    return JsonResponse(serialized_qs, safe=False)
+
+
+
+
+
 
