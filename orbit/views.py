1a6dae0027e5e8dc6684a3e2817e954f405d5981
gkiserpong <gkiserpongit@gmail.com>
2023-08-30 13:59:39 +0700

    Values specific fields

--- orbit/views.py
+++ orbit/views.py
@@ -6,23 +6,30 @@ from .models import Orbit, OrbitMulti
 
 
 def get_quota_all_orbit(request):
-    return JsonResponse(list(Orbit.objects.all().values()), safe=False)
+    return JsonResponse(list(Orbit.objects.all().values(
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+        )), safe=False)
 
 
 def get_quota_all_orbit_multi(request):
-    return JsonResponse(list(OrbitMulti.objects.all().values()), safe=False)
+    return JsonResponse(list(OrbitMulti.objects.all().values(
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+        )), safe=False)
 
 
 def get_quota(request, msisdn):
 
-    qs = Orbit.objects.filter(msisdn__contains=msisdn)
+    qs = Orbit.objects.filter(msisdn__contains=msisdn).values(
+            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+            )
 
     if not qs:
-        qs = OrbitMulti.objects.filter(msisdn__contains=msisdn)
+        qs = OrbitMulti.objects.filter(msisdn__contains=msisdn).values(
+                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+                )
 
     print(msisdn, qs)
-    serialized_qs = serialize('python', qs)
-    return JsonResponse(serialized_qs, safe=False)
+    return JsonResponse(list(qs), safe=False)
 
 
 
