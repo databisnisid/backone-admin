8efcb67d07002b1e09b0159d50b89b644d46c3c8
gkiserpong <gkiserpongit@gmail.com>
2023-09-25 15:12:02 +0700

    Add sleep cron orbit 5 minutes

--- orbit/cron.py
+++ orbit/cron.py
@@ -4,7 +4,7 @@ from .utils import get_all_quota_orbit, check_quota_orbit_notification_daily, ge
 
 def CRON_get_all_quota_orbit():
     get_all_quota_orbit_multi()
-    sleep(60)
+    sleep(300)
     get_all_quota_orbit()
     #get_all_quota_orbit_multi()
 
