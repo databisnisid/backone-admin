c99fd2d6cb1d27a6ade67ce497d2b5ab38e46081
gkiserpong <gkiserpongit@gmail.com>
2023-10-15 15:10:36 +0700

    Quota Previous Day API

--- orbit/views.py
+++ orbit/views.py
@@ -7,25 +7,25 @@ from .models import Orbit, OrbitMulti
 
 def get_quota_all_orbit(request):
     return JsonResponse(list(Orbit.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
         )), safe=False)
 
 
 def get_quota_all_orbit_multi(request):
     return JsonResponse(list(OrbitMulti.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
         )), safe=False)
 
 
 def get_quota(request, msisdn):
 
     qs = Orbit.objects.filter(msisdn__contains=msisdn).values(
-            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
             )
 
     if not qs:
         qs = OrbitMulti.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'updated_at'
+                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
                 )
 
     print(msisdn, qs)
