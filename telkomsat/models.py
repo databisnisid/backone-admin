282c6e66d0c1db3af33a3bde6203b267893b0c9f
gkiserpong <gkiserpongit@gmail.com>
2024-09-10 17:43:20 +0700

    Quota Date and Quota Day

--- telkomsat/models.py
+++ telkomsat/models.py
@@ -1,9 +1,11 @@
+import calendar
 from enum import unique
 from django.db import models
 from backone.models import BackOne
 from django.utils.translation import gettext_lazy as _
 from datetime import datetime
-from django.utils.timezone import now
+#from django.utils.timezone import now
+from django.utils import timezone
 
 class Starlink(models.Model):
     #service_line_number = models.CharField(max_length=50, unique=True, verbose_name=_('Service Line Number'))
@@ -16,7 +18,7 @@ class Starlink(models.Model):
     quota_date = models.DateField(_('Berlaku Sampai'), blank=True, null=True)
     quota_day =models.CharField(_('Kuota Hari'), max_length=20, blank=True, null=True)
     quota_prev = models.CharField(max_length=100, blank=True, null=True, verbose_name='Quota Prev')
-    activation_date = models.DateField(verbose_name=_('Tanggal Aktivasi'), blank=True, default=now())
+    activation_date = models.DateField(verbose_name=_('Tanggal Aktivasi'), blank=True, default=timezone.now())
 
     additional_info = models.TextField(blank=True, null=True, verbose_name='Keterangan')
     quota_type = models.CharField(max_length=20, default='starlink', verbose_name='Quota Type')
@@ -42,3 +44,13 @@ class Starlink(models.Model):
     def __str__(self):
         return '%s(%s/%s)' % (self.msisdn, self.quota_current, self.quota_until)
 
+    def save(self):
+
+        now = timezone.now().replace(tzinfo=None)
+        m = calendar.monthrange(now.year, now.month)
+        self.quota_date = timezone.datetime(now.year, now.month, m[1])
+
+        delta = self.quota_date - now
+        self.quota_day = str(delta.days) + ' Hari'
+
+        return super(Starlink, self).save()
