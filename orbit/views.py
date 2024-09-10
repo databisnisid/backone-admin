b56cdbf884aa541011b6a2473dbb3b9ff73d104f
gkiserpong <gkiserpongit@gmail.com>
2024-09-10 16:11:10 +0700

    get_quota Quota_Type

--- orbit/views.py
+++ orbit/views.py
@@ -8,30 +8,30 @@ from dsc.models import DscDpi
 
 def get_quota_all_orbit(request):
     return JsonResponse(list(Orbit.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
         )), safe=False)
 
 
 def get_quota_all_orbit_multi(request):
     return JsonResponse(list(OrbitMulti.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
+        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
         )), safe=False)
 
 
 def get_quota(request, msisdn):
 
     qs = Orbit.objects.filter(msisdn__contains=msisdn).values(
-            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
+            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
             )
 
     if not qs:
         qs = OrbitMulti.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
+                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
                 )
 
     if not qs:
         qs = DscDpi.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at'
+                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
                 )
 
     print(msisdn, qs)
