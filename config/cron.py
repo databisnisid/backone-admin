8f94b03e84d33fb4f300fb10321feb7d673de9be
proitlab <dedy.sutanto@proit.co.id>
2024-10-01 12:24:32 +0700

    DSC the last to get

--- config/cron.py
+++ config/cron.py
@@ -1,5 +1,8 @@
 from django.core.management import call_command
-from orbit.cron import CRON_get_all_quota_orbit, CRON_check_quota_orbit_notification_daily
+from orbit.cron import (
+    CRON_get_all_quota_orbit,
+    CRON_check_quota_orbit_notification_daily,
+)
 from dsc.utils import get_quota as dsc_gq
 from telkomsat.utils import get_quota as starlink_gq
 from time import sleep
@@ -7,18 +10,18 @@ from time import sleep
 
 def dbbackup_job():
     try:
-        call_command('dbbackup')
+        call_command("dbbackup")
 
     except:
         pass
+
 
 def get_all_quota():
     # Orbit
     CRON_get_all_quota_orbit()
     sleep(300)
-    # DSC
-    dsc_gq()
-    sleep(300)
     # Telkomsat
     starlink_gq()
-    
+    sleep(300)
+    # DSC
+    dsc_gq()
