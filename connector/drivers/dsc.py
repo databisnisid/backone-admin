30faba6d08f923bcecf3171c6cf3680a9b8e7b4c
proitlab <dedy.sutanto@proit.co.id>
2024-10-01 12:22:28 +0700

    Add delay = 60

--- connector/drivers/dsc.py
+++ connector/drivers/dsc.py
@@ -6,26 +6,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from django.conf import settings
 from connector.drivers import pop3
-from connector.drivers.selenium_utils import find_element_presence, find_element_clickable
+from connector.drivers.selenium_utils import (
+    find_element_presence,
+    find_element_clickable,
+)
 from time import sleep
 
 
-def check_for_error(driver: webdriver.Firefox) -> str:
+def check_for_error(driver: webdriver.Firefox, delay: int = 5) -> str:
 
-    delay = 1
     error_msg = ""
 
     # First Pop Up - Oops!
     try:
         elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Oops!')]"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//span[contains(text(), 'Oops!')]")
+            )
         )
 
         print("Found Oops!")
 
         # Get OK Button
         elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//button/span[contains(text(), 'OK')]"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//button/span[contains(text(), 'OK')]")
+            )
         )
 
         print("Click OK for Oops!")
@@ -34,17 +40,19 @@ def check_for_error(driver: webdriver.Firefox) -> str:
     except:
         pass
 
-    driver.implicitly_wait(1)
+    driver.implicitly_wait(delay)
 
     # Second Pop Up
 
     try:
         elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//img[@src='/images/icons/ico_error.png']"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//img[@src='/images/icons/ico_error.png']")
+            )
         )
-        #elem = WebDriverWait(driver, delay).until(
+        # elem = WebDriverWait(driver, delay).until(
         #    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Error')]"))
-        #)
+        # )
 
         elem = driver.find_elements(By.XPATH, "//span")
 
@@ -53,7 +61,9 @@ def check_for_error(driver: webdriver.Firefox) -> str:
 
         # Get OK Button
         elem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.XPATH, "//button/span[contains(text(), 'OK')]"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//button/span[contains(text(), 'OK')]")
+            )
         )
         print("Click OK")
         elem.click()
@@ -61,39 +71,47 @@ def check_for_error(driver: webdriver.Firefox) -> str:
     except:
         pass
 
-
     return error_msg
 
 
-def wait_for_loader(driver: webdriver.Firefox):
+def wait_for_loader(driver: webdriver.Firefox, delay: int = 60):
+
     try:
-        WebDriverWait(driver, 30).until(
-                EC.invisibility_of_element_located((By.XPATH, "//img[@src='/images/loader.gif']"))
+        WebDriverWait(driver, delay).until(
+            EC.invisibility_of_element_located(
+                (By.XPATH, "//img[@src='/images/loader.gif']")
+            )
         )
     except TimeoutException:
         driver.quit()
 
 
-def get_quota_value(driver: webdriver.Firefox) -> str:
+def get_quota_value(driver: webdriver.Firefox, delay: int = 60) -> str:
 
-    elem = find_element_presence(driver, 
-    #                             "//div[@class='MyProfilePackageQuotaCard-component__value___bBAlb']",
-                                 "//span[text()='MB' or text()='GB' or text()='KB']/..",
-                                 30, False)
+    elem = find_element_presence(
+        driver,
+        #                             "//div[@class='MyProfilePackageQuotaCard-component__value___bBAlb']",
+        "//span[text()='MB' or text()='GB' or text()='KB']/..",
+        delay,
+        False,
+    )
     quota_value = ""
     if elem:
         quota_value = elem.text
-        print("Quota Value: ",quota_value)
+        print("Quota Value: ", quota_value)
 
     return quota_value
 
 
-def get_quota_date(driver: webdriver.Firefox) -> str:
+def get_quota_date(driver: webdriver.Firefox, delay: int = 60) -> str:
 
-    elem = find_element_presence(driver, 
-    #                             "//div[@class='MyProfilePackageQuotaCard-component__date___y8Nm3']",
-                                 "//span[text()='Until']/..",
-                                 30, False)
+    elem = find_element_presence(
+        driver,
+        #                             "//div[@class='MyProfilePackageQuotaCard-component__date___y8Nm3']",
+        "//span[text()='Until']/..",
+        delay,
+        False,
+    )
 
     quota_date = ""
     if elem:
@@ -108,17 +126,17 @@ def search_by_msisdn(driver: webdriver.Firefox, msisdns: list = []) -> dict:
     if not len(msisdns):
         return {}
 
-    delay: int = 30
+    delay: int = 60
     error_msg: str = ""
 
     result = {}
 
     for msisdn in msisdns:
 
-        """ input mobileNumber """
-        elem = find_element_presence(driver,
-                                     "//input[@name='mobileNumber']",
-                                     delay, True)
+        """input mobileNumber"""
+        elem = find_element_presence(
+            driver, "//input[@name='mobileNumber']", delay, True
+        )
         if elem:
             elem.clear()
             elem.send_keys(msisdn)
@@ -133,28 +151,30 @@ def search_by_msisdn(driver: webdriver.Firefox, msisdns: list = []) -> dict:
 
         if error_msg == "":
 
-            """ get Quota Info """
+            """get Quota Info"""
             quota_value = get_quota_value(driver)
 
             """ get Quota Date """
             quota_date = get_quota_date(driver)
 
-
         msisdn_result = {
-                msisdn : {
-                    'quota_value': quota_value,
-                    'quota_date': quota_date,
-                    'error_msg': error_msg
-                    }
-                }
+            msisdn: {
+                "quota_value": quota_value,
+                "quota_date": quota_date,
+                "error_msg": error_msg,
+            }
+        }
         result.update(msisdn_result)
 
-
-    print('Search By MSISDN is done')
+    print("Search By MSISDN is done")
     return result
 
 
-def login_to_dsc(msisdns: list = [], username: str = settings.DSC_USERNAME, password: str = settings.DSC_PASSWORD) -> dict:
+def login_to_dsc(
+    msisdns: list = [],
+    username: str = settings.DSC_USERNAME,
+    password: str = settings.DSC_PASSWORD,
+) -> dict:
 
     if not len(msisdns):
         return {}
@@ -165,42 +185,35 @@ def login_to_dsc(msisdns: list = [], username: str = settings.DSC_USERNAME, pass
 
     if username is not None and password is not None:
 
-        print('Use Selenium Server: ', settings.REMOTE_SELENIUM)
+        print("Use Selenium Server: ", settings.REMOTE_SELENIUM)
         if settings.REMOTE_SELENIUM:
             driver_options = webdriver.FirefoxOptions()
 
             driver = webdriver.Remote(
-                command_executor=settings.SELENIUM_DOCKER,
-                options=driver_options
+                command_executor=settings.SELENIUM_DOCKER, options=driver_options
             )
         else:
             driver = webdriver.Firefox()
 
         driver.get("https://dsc.telkomsel.com")
-        delay = 30
-
+        delay = 60
 
         """ Sending Username """
-        elem = find_element_presence(driver, 
-                                     "//input[@name='mobileNumberOrEmail']",
-                                     delay, True)
+        elem = find_element_presence(
+            driver, "//input[@name='mobileNumberOrEmail']", delay, True
+        )
         if elem:
             elem.send_keys(username)
             elem.send_keys(Keys.RETURN)
 
-
         """ Check Term and Condition """
-        elem = find_element_clickable(driver, 
-                                      "//input[@id='acceptTnc']",
-                                      delay, True)
+        elem = find_element_clickable(driver, "//input[@id='acceptTnc']", delay, True)
         if elem:
             wait_for_loader(driver)
             elem.click()
 
         """ Click GET CODE Button """
-        elem = find_element_clickable(driver, 
-                                     "//button[@type='button']",
-                                     delay, True)
+        elem = find_element_clickable(driver, "//button[@type='button']", delay, True)
 
         if elem:
             wait_for_loader(driver)
@@ -210,7 +223,7 @@ def login_to_dsc(msisdns: list = [], username: str = settings.DSC_USERNAME, pass
         Function to get Code from email
         Use poplib 
         """
-        for i in range(1,25):
+        for i in range(1, 25):
             print("Trying get code #" + str(i))
             otp_code = pop3.get_otp_code()
             if otp_code != "":
@@ -219,44 +232,37 @@ def login_to_dsc(msisdns: list = [], username: str = settings.DSC_USERNAME, pass
             sleep(3)
 
         """ Enter CODE """
-        elem = find_element_presence(driver,
-                                     "//input[@name='otp']",
-                                     delay, True)
+        elem = find_element_presence(driver, "//input[@name='otp']", delay, True)
 
         if elem:
             elem.send_keys(otp_code)
 
         """ SUBMIT """
-        elem = find_element_clickable(driver,
-                                      "//button[@type='submit']",
-                                      delay, True)
+        elem = find_element_clickable(driver, "//button[@type='submit']", delay, True)
         if elem:
             wait_for_loader(driver)
             elem.click()
 
-
         """ myServices """
-        elem = find_element_clickable(driver,
-                                      "//div[@id='myServices']",
-                                      delay, True)
+        elem = find_element_clickable(driver, "//div[@id='myServices']", delay, True)
         if elem:
             wait_for_loader(driver)
             elem.click()
             print("Click myServices")
 
         """ checkmember """
-        elem = find_element_clickable(driver,
-                                      "//a[@href='/pic/check-member']",
-                                      delay, True)
+        elem = find_element_clickable(
+            driver, "//a[@href='/pic/check-member']", delay, True
+        )
         if elem:
             wait_for_loader(driver)
             elem.click()
             print("Click Check Member")
 
-        #msisdns = ['6281117064193', '62811170668622', '6281117064195']
+        # msisdns = ['6281117064193', '62811170668622', '6281117064195']
         result = search_by_msisdn(driver, msisdns)
 
-        print('So Farrr...Quit')
+        print("So Farrr...Quit")
 
         driver.quit()
 
