6b7c2bea1c6006c47e6ba87d1f52853bf7ec3443
proitlab <dedy.sutanto@proit.co.id>
2024-10-15 10:14:46 +0700

    DPI MSDISN unik

--- dsc/models.py
+++ dsc/models.py
@@ -3,27 +3,35 @@ from backone.models import BackOne
 from django.utils.translation import gettext_lazy as _
 from datetime import datetime
 
+
 class DscDpi(models.Model):
-    msisdn = models.CharField(max_length=30, verbose_name='MSISDN')
-    imei = models.CharField(max_length=30, verbose_name='IMEI', blank=True)
-    quota_total = models.CharField(max_length=20, blank=True, verbose_name='Kuota Total', default='5 GB')
-    quota_current = models.CharField(max_length=20, blank=True, verbose_name='Kuota Saat Ini')
-    quota_until = models.CharField(max_length=20, blank=True, verbose_name='Kuota Masa Berlaku ')
-    quota_date = models.DateField(_('Berlaku Sampai'), blank=True, null=True)
-    quota_day =models.CharField(_('Kuota Hari'), max_length=20, blank=True, null=True)
-    quota_prev = models.CharField(max_length=100, blank=True, null=True, verbose_name='Quota Prev')
+    msisdn = models.CharField(max_length=30, verbose_name="MSISDN", unique=True)
+    imei = models.CharField(max_length=30, verbose_name="IMEI", blank=True)
+    quota_total = models.CharField(
+        max_length=20, blank=True, verbose_name="Kuota Total", default="5 GB"
+    )
+    quota_current = models.CharField(
+        max_length=20, blank=True, verbose_name="Kuota Saat Ini"
+    )
+    quota_until = models.CharField(
+        max_length=20, blank=True, verbose_name="Kuota Masa Berlaku "
+    )
+    quota_date = models.DateField(_("Berlaku Sampai"), blank=True, null=True)
+    quota_day = models.CharField(_("Kuota Hari"), max_length=20, blank=True, null=True)
+    quota_prev = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Quota Prev"
+    )
 
-    additional_info = models.TextField(blank=True, null=True, verbose_name='Keterangan')
-    error_msg = models.CharField(_('Error Message'), max_length=100, blank=True, null=True)
-    quota_type = models.CharField(max_length=20, default='dpi', verbose_name='Quota Type')
-
+    additional_info = models.TextField(blank=True, null=True, verbose_name="Keterangan")
+    error_msg = models.CharField(
+        _("Error Message"), max_length=100, blank=True, null=True
+    )
+    quota_type = models.CharField(
+        max_length=20, default="dpi", verbose_name="Quota Type"
+    )
 
     site = models.OneToOneField(
-        BackOne,
-        on_delete=models.RESTRICT,
-        null=True,
-        blank=True,
-        verbose_name='Site'
+        BackOne, on_delete=models.RESTRICT, null=True, blank=True, verbose_name="Site"
     )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -31,21 +39,21 @@ class DscDpi(models.Model):
 
     class Meta:
         managed = True
-        db_table = 'dscdpi'
-        verbose_name = 'DPI'
-        verbose_name_plural = 'Daftar DPI'
+        db_table = "dscdpi"
+        verbose_name = "DPI"
+        verbose_name_plural = "Daftar DPI"
 
     def __str__(self):
-        return '%s(%s/%s)' % (self.msisdn, self.quota_current, self.quota_until)
+        return "%s(%s/%s)" % (self.msisdn, self.quota_current, self.quota_until)
 
     def save(self):
         if self.quota_until != "":
-            date_format = '%d/%m/%Y'
+            date_format = "%d/%m/%Y"
             try:
                 self.quota_date = datetime.strptime(self.quota_until, date_format)
 
                 delta = self.quota_date - datetime.now()
-                self.quota_day = str(delta.days) + ' Hari'
+                self.quota_day = str(delta.days) + " Hari"
 
             except ValueError:
                 pass
@@ -54,17 +62,16 @@ class DscDpi(models.Model):
 
 
 class DscDpiStatQuota(models.Model):
-    msisdn = models.CharField(max_length=30, verbose_name='MSISDN')
+    msisdn = models.CharField(max_length=30, verbose_name="MSISDN")
     quota = models.IntegerField()
     quota_string = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     class Meta:
         managed = True
-        db_table = 'dscdpi_stat'
-        verbose_name = 'DPI Statistic'
-        verbose_name_plural = 'DPI Statistics'
+        db_table = "dscdpi_stat"
+        verbose_name = "DPI Statistic"
+        verbose_name_plural = "DPI Statistics"
 
     def __str__(self):
-        return '{}'.format(self.msisdn)
-
+        return "{}".format(self.msisdn)
