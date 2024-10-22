37a42c4f55c3ca35b11839ceda915f02ba36933c
proitlab <dedy.sutanto@proit.co.id>
2024-10-22 11:35:26 +0700

    Add DscDpiProit in api call

--- orbit/views.py
+++ orbit/views.py
@@ -3,47 +3,104 @@ from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Orbit, OrbitMulti
-from dsc.models import DscDpi
+from dsc.models import DscDpi, DscDpiProit
 from telkomsat.models import Starlink
 
 
 def get_quota_all_orbit(request):
-    return JsonResponse(list(Orbit.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-        )), safe=False)
+    return JsonResponse(
+        list(
+            Orbit.objects.all().values(
+                "id",
+                "msisdn",
+                "quota_total",
+                "quota_current",
+                "quota_day",
+                "quota_prev",
+                "updated_at",
+                "quota_type",
+            )
+        ),
+        safe=False,
+    )
 
 
 def get_quota_all_orbit_multi(request):
-    return JsonResponse(list(OrbitMulti.objects.all().values(
-        'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-        )), safe=False)
+    return JsonResponse(
+        list(
+            OrbitMulti.objects.all().values(
+                "id",
+                "msisdn",
+                "quota_total",
+                "quota_current",
+                "quota_day",
+                "quota_prev",
+                "updated_at",
+                "quota_type",
+            )
+        ),
+        safe=False,
+    )
 
 
 def get_quota(request, msisdn):
 
     qs = Orbit.objects.filter(msisdn__contains=msisdn).values(
-            'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-            )
+        "id",
+        "msisdn",
+        "quota_total",
+        "quota_current",
+        "quota_day",
+        "quota_prev",
+        "updated_at",
+        "quota_type",
+    )
 
     if not qs:
         qs = OrbitMulti.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-                )
+            "id",
+            "msisdn",
+            "quota_total",
+            "quota_current",
+            "quota_day",
+            "quota_prev",
+            "updated_at",
+            "quota_type",
+        )
 
     if not qs:
         qs = DscDpi.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-                )
+            "id",
+            "msisdn",
+            "quota_total",
+            "quota_current",
+            "quota_day",
+            "quota_prev",
+            "updated_at",
+            "quota_type",
+        )
 
     if not qs:
+        qs = DscDpiProit.objects.filter(msisdn__contains=msisdn).values(
+            "id",
+            "msisdn",
+            "quota_total",
+            "quota_current",
+            "quota_day",
+            "quota_prev",
+            "updated_at",
+            "quota_type",
+        )
+    if not qs:
         qs = Starlink.objects.filter(msisdn__contains=msisdn).values(
-                'id', 'msisdn', 'quota_total', 'quota_current', 'quota_day', 'quota_prev', 'updated_at', 'quota_type'
-                )
+            "id",
+            "msisdn",
+            "quota_total",
+            "quota_current",
+            "quota_day",
+            "quota_prev",
+            "updated_at",
+            "quota_type",
+        )
     print(msisdn, qs)
     return JsonResponse(list(qs), safe=False)
-
-
-
-
-
-
